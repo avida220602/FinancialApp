29082610e9ecfdca178858e734145d4c0279d524
avida220602 <avidafaiza33@students.amikom.ac.id>
2022-04-08 23:02:06 +0700

    Second Commit

--- app/src/main/java/com/praktikum/financialapp/profile/ProfileActivity.kt
+++ app/src/main/java/com/praktikum/financialapp/profile/ProfileActivity.kt
@@ -2,11 +2,37 @@ package com.praktikum.financialapp.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.praktikum.financialapp.R
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        title = "Menu Profile"
+        val nama = findViewById<TextView>(R.id.tvnama)
+        val nim = findViewById<TextView>(R.id.tvnim)
+        val mail = findViewById<TextView>(R.id.tvemail)
+        val nope = findViewById<TextView>(R.id.tvnohp)
+        val jk = findViewById<TextView>(R.id.tvjk)
+        val prodi = findViewById<TextView>(R.id.tvprogramstudi)
+        val org = findViewById<TextView>(R.id.tvorganisasi)
+        val status = findViewById<TextView>(R.id.tvstatus)
+
+        val bukanbundle = intent.extras
+        if (bukanbundle != null){
+            nama.setText(bukanbundle.getString("nama"))
+            nim.setText(bukanbundle.getString("nim"))
+            mail.setText(bukanbundle.getString("mail"))
+            nope.setText(bukanbundle.getString("nope"))
+            jk.setText(bukanbundle.getString("jenkel"))
+            prodi.setText(bukanbundle.getString("prodi"))
+            org.setText(bukanbundle.getString("org"))
+            status.setText(bukanbundle.getString("status"))
+        }
     }
 }
+
+
+
--- app/src/main/java/com/praktikum/financialapp/setting/SettingActivity.kt
+++ app/src/main/java/com/praktikum/financialapp/setting/SettingActivity.kt
@@ -1,12 +1,83 @@
 package com.praktikum.financialapp.setting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import com.praktikum.financialapp.R
+import com.praktikum.financialapp.profile.ProfileActivity
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
+        title = "Menu Setting"
+        val btnsave = findViewById<Button>(R.id.btnsimpan)
+        val nama = findViewById<EditText>(R.id.etnamalkp)
+        val nim = findViewById<EditText>(R.id.etnim)
+        val mail = findViewById<EditText>(R.id.etemail)
+        val nope = findViewById<EditText>(R.id.ethp)
+        val pass = findViewById<EditText>(R.id.etpassword)
+        val rgjk = findViewById<RadioGroup>(R.id.rgjenkel)
+        val spiner = findViewById<Spinner>(R.id.spinner)
+        val amcc = findViewById<CheckBox>(R.id.amcc)
+        val hmmsi = findViewById<CheckBox>(R.id.himssi)
+        val bem = findViewById<CheckBox>(R.id.bemamikom)
+        val toggleButton = findViewById<ToggleButton>(R.id.togglebtn)
+
+        btnsave.setOnClickListener {
+            val cekjk = rgjk.checkedRadioButtonId
+            val jk = findViewById<RadioButton>(cekjk)
+
+            val org = StringBuilder()
+            if (amcc.isChecked) {
+                org.append("AMCC\n")
+            }
+            if (hmmsi.isChecked) {
+                org.append("HMSSI\n")
+            }
+            if (bem.isChecked) {
+                org.append("BEM AMIKOM\n")
+            }
+
+            val bundle = Bundle()
+            bundle.putString("nama", nama.text.toString())
+            bundle.putString("nim", nim.text.toString())
+            bundle.putString("mail", mail.text.toString())
+            bundle.putString("nope", nope.text.toString())
+            bundle.putString("jkel", "${jk.text}")
+            bundle.putString("prodi", spiner.selectedItem.toString())
+            bundle.putString("org", org.toString())
+            bundle.putString("status", toggleButton.text.toString())
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(mail.text.toString()).matches()) {
+                mail.error = "Masukkan Email Dengan Benar"
+            } else if (pass.length() == 0) {
+                pass.error = "Password Tidak Boleh Kosong"
+            } else {
+                val alertDialogBuilder = AlertDialog.Builder(this)
+                alertDialogBuilder.setTitle("Peringatan!!!!")
+                alertDialogBuilder.setMessage("Apakah Data Anda Sudah Benar?")
+                    .setCancelable(false)
+                    .setPositiveButton(
+                        "YES"
+                    ) { dialogInterface, i ->
+                        val intent = Intent(this, ProfileActivity::class.java)
+                        intent.putExtras(bundle)
+                        startActivity(intent)
+                        Toast.makeText(this, "Data Profil Berhasil Tersimpan", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                    .setNegativeButton(
+                        "NO"
+                    ) { dialogInterface, i -> dialogInterface.cancel() }
+                val alertDialog = alertDialogBuilder.create()
+                alertDialog.show()
+
+            }
+        }
     }
 }
